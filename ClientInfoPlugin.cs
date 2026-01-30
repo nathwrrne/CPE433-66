f66971627e1fc8ebf130bc9e489929a60a2da730
Natthawan <natthawan_s@cmu.ac.th>
2026-01-30 17:35:01 +0700

    Add ClientInfoPlugin

--- ClientInfoPlugin.cs
+++ ClientInfoPlugin.cs
@@ -31,28 +31,34 @@ namespace DNWS
 
     public HTTPResponse GetResponse(HTTPRequest request)
     {
-      HTTPResponse response = null;
       StringBuilder sb = new StringBuilder();
 
       IPEndPoint endpoint = IPEndPoint.Parse(request.getPropertyByKey("remoteendpoint"));
+
+      //Add header
+      string userAgent = request.getPropertyByKey("user-agent") ?? "N/A";
+      string acceptLang = request.getPropertyByKey("accept-language") ?? "N/A";
+      string acceptEnc = request.getPropertyByKey("accept-encoding") ?? "N/A";
+
       sb.Append("<html><body><pre>");
       sb.AppendFormat("Client IP: {0}<br/>\n", endpoint.Address);
       sb.AppendFormat("Client Port: {0}<br/>\n", endpoint.Port);
-      sb.AppendFormat("Browser Information: {0}<br/>\n", request.getPropertyByKey("user-agent").Trim());
-      sb.AppendFormat("Accept Language: {0}<br/>\n", request.getPropertyByKey("accept-language").Trim());
-      sb.AppendFormat("Accept Encoding: {0}<br/>\n", request.getPropertyByKey("accept-encoding").Trim());
+      //Remove .Trim
+      sb.AppendFormat("Browser Information: {0}<br/>\n", userAgent);
+      sb.AppendFormat("Accept Language: {0}<br/>\n", acceptLang);
+      sb.AppendFormat("Accept Encoding: {0}<br/>\n", acceptEnc);
 
       sb.Append("</pre></body></html>");
 
-      response = new HTTPResponse(200);
+      HTTPResponse response = new HTTPResponse(200);
       response.body = Encoding.UTF8.GetBytes(sb.ToString());
       return response;
     }
 
-
     public HTTPResponse PostProcessing(HTTPResponse response)
     {
-      throw new NotImplementedException();
+      // throw new NotImplementedException(); 
+      return response;
     }
   }
 }
